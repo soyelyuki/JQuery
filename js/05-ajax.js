1d9198e4a75536cf29bb66e78dcb28f294bd3915
Nicolas Miranda Meza <nicolas@pop-os.localdomain>
2020-08-04 10:53:09 -0400

    JQuery

--- js/05-ajax.js
+++ js/05-ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    console.log("hola")
+    console.log("Usando Jquery correctamente")
     //load
 
    // $("#datos").load("https://reqres.in/")
@@ -11,4 +11,36 @@ $(document).ready(function(){
             $("#datos").append("<p>"+ element.first_name+" "+ element.last_name+"<p>");
         });
     });
-});
+    
+    $("#formulario").submit(function(e){
+        e.preventDefault(); //evita que se cambie la pagina
+        var usuario = {
+            name:$('input[name="name"]').val(),
+            web:$('input[web="web"]').val(),
+        };
+        /*
+        $.post($(this).attr("action"),usuario,function(response){
+            console.log(response);
+        }).done(function(){
+            alert("Usuarion añadido exitosamente");
+        */
+        $.ajax({
+            type:'POST',
+            url:$(this).attr("action"),
+            data: usuario,
+            beforeSend: function(){
+                console.log("enviando usuario...");
+            },
+            success: function(response){
+                console.log(response);
+            },
+            error: function(){
+                console.log("a ocurrido un error");
+            },
+            timeout: 2000
+        });
+        
+    });
+
+        return false;
+    });
