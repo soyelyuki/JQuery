170f89c0a4a11806900df5eb05add17812f3e0c6
Nicolas Miranda Meza <nicolas@pop-os.localdomain>
2020-08-06 10:01:24 -0400

    Jquery

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-    console.log("JQ Cargado");
+
+    //Mover elemento por la pagina
+    $(".elemento").draggable();
+
+    //redimencionar
+
+    $(".elemento").resizable();
+
+    //seleccionar elementos
+    //$(".lista-seleccionable").selectable();
+
+    //seleccionar y ordenar 
+    $(".lista-seleccionable").sortable({
+        update: function(event, ui){
+            console.log("ha cambiado la lista");
+        }
+    });    
+    //drag n drop
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("has insertado algo en el area");
+        }
+    });
+    $("#mostrar").click(function(){
+        $(".caja-efectos").toggle("explode");
+        //efectos togle, explode, drop, fold,y monton deefectos
+        
+    }) 
 });
